bb4a636cdc26c1f04091fdfef464a5380ac39997
Alex QAsper <semidog@gmail.com>
2022-11-06 18:57:26 +0500

    add redirect by id

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('<zodiac_sign>/', views.get_zodiac_info),
+    #path('<zodiac_sign>/', views.get_zodiac_info)
+    path('<int:zodiac_sign>/', views.get_zodiac_number_info),
+    path('<str:zodiac_sign>/', views.get_zodiac_info)
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 dict_zodiac = {
     'leo': 'Знак зодиака - ЛЕВ!',
@@ -10,9 +10,17 @@ dict_zodiac = {
 }
 
 
-def get_zodiac_info(request, zodiac_sign):
+def get_zodiac_info(request, zodiac_sign: str):
     description = dict_zodiac.get(zodiac_sign, None)
     if description:
         return HttpResponse(description)
     else:
         return HttpResponseNotFound(f'Не найдена команда {zodiac_sign}')
+
+
+def get_zodiac_number_info(request, zodiac_sign: int):
+    zodiacs = list(dict_zodiac)
+    if zodiac_sign > len(zodiacs):
+        return HttpResponseNotFound(f'Неправильный номер знака зодиака')
+    name_zodiac = zodiacs[zodiac_sign - 1]
+    return HttpResponseRedirect(f'/horoscope/{name_zodiac}')
