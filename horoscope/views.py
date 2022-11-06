f27c91e224a07daefae57c489bed3f6d9b799019
Alex QAsper <semidog@gmail.com>
2022-11-06 17:48:18 +0500

    add dynamic URLs from dictionary method

--- horoscope/views.py
+++ horoscope/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+dict_zodiac = {
+    'leo': 'Знак зодиака - ЛЕВ!',
+    'aries': 'Овен (Aries) 21 марта – 20 апреля',
+    'taurus': 'Телец (Taurus) 21 апреля – 20 мая',
+    'gemini': 'Близнецы (Gemini) 21 мая – 21 июня',
+    'cancer': 'Рак (Cancer) 22 июня – 22 июля'
+}
+
+
 def get_zodiac_info(request, zodiac_sign):
-    if zodiac_sign == 'leo':
-        return HttpResponse('Знак зодиака - ЛЕВ!')
-    elif zodiac_sign == 'aries':
-        return HttpResponse('Овен (Aries) 21 марта – 20 апреля')
-    elif zodiac_sign == 'taurus':
-        return HttpResponse('Телец (Taurus) 21 апреля – 20 мая')
-    elif zodiac_sign == 'gemini':
-        return HttpResponse('Близнецы (Gemini) 21 мая – 21 июня')
-    elif zodiac_sign == 'cancer':
-        return HttpResponse('Рак (Cancer) 22 июня – 22 июля')
+    description = dict_zodiac.get(zodiac_sign, None)
+    if description:
+        return HttpResponse(description)
     else:
         return HttpResponseNotFound(f'Не найдена команда {zodiac_sign}')
-
