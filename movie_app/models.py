e8583969f63d19ad46cde4e280fd1744ae3cef9b
Alex QAsper <semidog@gmail.com>
2022-11-12 19:06:11 +0500

    Try add, update, delete in ORM Django

--- movie_app/models.py
+++ movie_app/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Movies(models.Model):
     name = models.CharField(max_length=30)
     rating = models.IntegerField()
+    year = models.IntegerField(null=True)
+    budget = models.IntegerField(default=1000000)
+
+    def __str__(self):
+        return f'{self.name} - {self.rating}%'
